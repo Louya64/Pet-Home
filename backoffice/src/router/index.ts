76baae825b01f3668923539605c05d91a4a6b97f
Louya64 <cixie@live.fr>
2022-04-25 09:28:24 +0200

    admin can login and logout from dashbord (functionnal)

--- backoffice/src/router/index.ts
+++ backoffice/src/router/index.ts
@@ -1,23 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginViewVue from "@/views/LoginView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/dashboard",
+			name: "home",
+			component: HomeView,
+		},
+		{
+			path: "/dashboard/login",
+			name: "login",
+			component: LoginViewVue,
+		},
+	],
+});
 
-export default router
+// router.beforeEach(async (to, from) => {
+// 	const isAuthenticate = localStorage.getItem("token");
+// 	if (!isAuthenticate) {
+// 		return { name: "login" };
+// 	}
+// 	if (isAuthenticate && to.name === "login") {
+// 		return { name: "postList" };
+// 	}
+// });
+
+export default router;
